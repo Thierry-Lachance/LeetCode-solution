51d017e3395e62cbef9176c71c759e8c8fd41c3f
Thierry Lachance <101730754+Thierry-Lachance@users.noreply.github.com>
2024-01-04 18:18:59 -0500

    Create 217-Contains_Duplicates.java

---
+++ 217-Contains_Duplicates.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList; // import the ArrayList class
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums); 
+        for(int i = 1; i < nums.length; i++){
+           if(nums[i] == nums[(i-1)]){
+              return true;
+           }
+        }
+        return false;
+    }
+}
