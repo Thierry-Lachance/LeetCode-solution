5ee0969bf337bee798d0c75d3fa0751408c37c6c
Thierry Lachance <lachancethierry6@gmail.com>
2024-11-20 13:29:04 -0500

    Create 3024.java

---
+++ 3024.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String triangleType(int[] nums) {
+
+        Arrays.sort(nums);
+        if (nums[0] + nums[1] <= nums[2])
+            return "none";
+        if (nums[0] == nums[2])
+            return "equilateral";
+        if (nums[0] == nums[1])
+            return "isosceles";
+        if (nums[1] == nums[2])
+            return "isosceles";
+        else
+            return "scalene";
+    }
+}
